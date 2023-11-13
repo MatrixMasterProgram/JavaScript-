ffaafd2bd45109cb73608699c37c1c1799b38090
Bashar Alessa <113999183+BasharAlessa@users.noreply.github.com>
2023-11-13 10:42:35 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+// console.log("Hello world")
+// console.log("this is the script from html")
+// variables where can save your temp data on the pc 
+
+var name = "Alice"
+var age = 28
+console.log(name);
+console.log(age);
+console.log(typeof name);
+console.log(typeof age);
+
+var isOld = false
+console.log(isOld);
+
+var num1=10
+var num2=20
+console.log(num1 / num2);
+
+//conditionals if , else if , else
+var Age = 18
+if(Age >=20){
+    console.log("you are adult");
+} else {
+    console.log("you are not an adult");
+}
+
+var grade = 75
+
+if(grade >=90){
+    console.log("A");
+}else if(grade >=80){
+    console.log("B");
+}else if (grade >=70){
+    console.log("C");
+}else {
+    console.log("D");
+}
+// && ||
+var isSunny = false
+var isWarm = true
+if(isSunny || isWarm){
+    console.log("its a perfect day");
+} else{
+    console.log("not a perfect day to go out");
+}
+
+

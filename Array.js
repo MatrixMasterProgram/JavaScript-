faf30bb5dcbf210f3b0b90851c7575b34ec33865
Bashar Alessa <113999183+BasharAlessa@users.noreply.github.com>
2023-11-19 11:47:25 +0100

    Add files via upload

---
+++ Array.js
@@ -0,0 +1,33 @@
+// array 
+
+let car0 ="BMW"
+let car1 = "Mazda"
+let car2 = "Merceds"
+
+const cars =["BMW", "Mazada" , "Merceds"]
+console.log(cars);
+
+console.log(cars[0]);
+// index[0]
+// index [1]
+
+// type of array
+// types of data
+// String
+// Object
+// number
+console.log(typeof cars);
+console.log(cars[1]);
+cars[3]="Volvo"
+console.log(cars);
+cars[6]="Opel"
+console.log(cars);
+console.log(cars.length);
+cars[0]= "Ford"
+console.log(cars);
+cars[0]=Date()
+console.log(cars);
+function myFuncation(){
+    const x= []
+    return x
+}
